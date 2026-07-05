d2176b93b2ac8d7818e6a77bb776f74f0522d2f8
BantonOrg <faraman5111@gmail.com>
2026-07-05 13:32:43 +0100

    Update game.js

--- ludo/game.js
+++ ludo/game.js
@@ -19,9 +19,9 @@ const TURN_TIMEOUT_SECONDS = 30;
 
 const COLORS = ["red", "green", "yellow", "blue"];
 
-// 15x15 Grid Layout Pathing Array (Fixed Global Tracking Map)
+// 15x15 Grid Layout Pathing Array (Clockwise Tracking Map)
 const COMMON_TRACK = [
-  {x:6, y:1}, {x:6, y:2}, {x:6, y:3}, {x:6, y:4}, {x:6, y:5},
+  {x:6, y:0}, {x:6, y:1}, {x:6, y:2}, {x:6, y:3}, {x:6, y:4}, {x:6, y:5},
   {x:5, y:6}, {x:4, y:6}, {x:3, y:6}, {x:2, y:6}, {x:1, y:6}, {x:0, y:6},
   {x:0, y:7},
   {x:0, y:8}, {x:1, y:8}, {x:2, y:8}, {x:3, y:8}, {x:4, y:8}, {x:5, y:8},
@@ -32,7 +32,7 @@ const COMMON_TRACK = [
   {x:14, y:7},
   {x:14, y:6}, {x:13, y:6}, {x:12, y:6}, {x:11, y:6}, {x:10, y:6}, {x:9, y:6},
   {x:8, y:5}, {x:8, y:4}, {x:8, y:3}, {x:8, y:2}, {x:8, y:1}, {x:8, y:0},
-  {x:7, y:0}, {x:6, y:0}
+  {x:7, y:0}
 ];
 
 // Re-aligned perfectly to match your background image boxes
